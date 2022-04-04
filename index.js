3de0358f905484304dedc6cbe6d9927ad90a408a
Alexander Bonilla <aldaboad@outlook.com>
2022-04-04 10:36:20 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send(`
+	<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='UTF-8'>
+    <script src="https://unpkg.com/react@17/umd/react.production.min.js" crossorigin></script>
+    <script src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js" crossorigin></script>
+    <script src='https://unpkg.com/babel-standalone@6.26.0/babel.js'></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>hello</title>
+</head>
+
+<body>
+    <div id='root'></div>
+
+    <script type='text/babel'>
+       
+        function App() {     
+            return (
+                    <section className="w-screen h-screen flex items-center justify-center">
+                        <header className=" shadow-md rounded-md bg-blue-500 p-4">
+
+                           <h1 className="font-bold text-white">Hello Heroku and docker app</h1>
+                        </header>
+                    </section>
+            )
+        }
+
+
+        ReactDOM.render(<App />, document.getElementById('root'));
+    </script>
+</body>
+</html>
+  `);
+})
+
+app.listen(port, () => {
+  console.log('\x1b[32m\x1b[2mApp listening\x1b[0m');
+  console.log(`\x1b[32mPort:\x1b[0m ${port}`);
+})
